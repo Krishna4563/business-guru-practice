8a8b58f3b3a02a1cce43d0adbe2a48bcb2c2d803
Krishna4563 <daskrishna0902@gmail.com>
2024-03-18 17:31:16 +0530

    client and server partition

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Carousel from "./components/carousel";
+import HomeImg1 from "./assets/home-img-1.avif";
+import HomeImg2 from "./assets/home-img-2.avif";
+import HomeImg3 from "./assets/home-img-3.avif";
+import Home from "./components/home";
+import Form from "./components/form";
+
+const App = () => {
+  const images = [HomeImg1, HomeImg2, HomeImg3];
+
+  return (
+    <div>
+      <Carousel images={images} />
+      <Home />
+      <Form />
+    </div>
+  );
+};
+
+export default App;
